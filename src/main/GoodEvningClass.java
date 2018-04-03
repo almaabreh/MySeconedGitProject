c729bf66a8cc6a9b53d50d6ad0cf4a9de5a1cfdd
alamaabreh <alamaabreh@10.0.0.193>
2018-04-03 10:50:58 -0400

    new print statment

--- src/main/GoodEvningClass.java
+++ src/main/GoodEvningClass.java
@@ -3,7 +3,9 @@ package main;
 public class GoodEvningClass {
 
 	public static void main(String[] args) {
-		System.out.println("Good Evenig Class");
+		System.out.println("Good Evening Class");
+		
+		System.out.println("my name ala almaabreh");
 		
 	}
 
--- src/main/GoodMorningClass.java
+++ src/main/GoodMorningClass.java
@@ -3,8 +3,8 @@ package main;
 public class GoodMorningClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("Good morning Class");
 	}
 
 }
